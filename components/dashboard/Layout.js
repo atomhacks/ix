ed3c8197f9932098e372b4668b16951742951889
Fahim Hussain <fahimhussain21@gmail.com>
2023-03-03 00:21:32 -0500

    oops

--- components/dashboard/Layout.js
+++ components/dashboard/Layout.js
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-neutral-800 font-montserrat text-white">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="ml-56 pt-4 pl-4">{children}</div>
     </div>
   );
