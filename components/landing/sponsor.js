88678d2aa0115d03e43c6a9fa9c54cb636c8fda8
warren yun <wyun2006@gmail.com>
2023-03-03 13:23:36 -0500

    wee sponsor

--- components/landing/sponsor.js
+++ components/landing/sponsor.js
@@ -1,29 +1,49 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const list = [
   {
-    name: "Small Planet",
-    path: "sp-logo.svg",
-    link: "",
+    name: "Taskade",
+    path: "taskade-logo.png",
+    link: "https://www.taskade.com/",
   },
   {
     name: "Echo3D",
     path: "echo3d-logo.png",
-    link: "",
+    link: "https://www.echo3d.com/",
+  },
+  {
+    name: "Hudson River Trading",
+    path: "hrt-logo.svg",
+    link: "https://www.hudsonrivertrading.com/",
+  },
+  {
+    name: "Small Planet",
+    path: "sp-logo.svg",
+    link: "https://smallplanet.com/",
+  },
+  {
+    name: "XYZ Domains",
+    path: "xyz-logo.png",
+    link: "https://www.taskade.com/",
   },
 ];
 
 const Sponsors = () => {
   return (
-    <div id="#sponsors" className="flex flex-col items-center justify-center py-24 border-b-8 border-green-500 ">
+    <div id="sponsors" className="flex flex-col items-center justify-center py-24 border-b-8 border-green-500 ">
       <div className="flex items-center justify-center mb-20">
         <span className="py-6 border-b-4 border-yellow-500 md:text-5xl text-7xl font-morro">SPONSORS</span>
       </div>
-      <h1 className="mb-20 text-xl md:text-center md:px-4 md:mb-12 md:text-base font-montserrat">Thanks to our amazing sponsors for making AtomHacks possible!</h1>
+      <h1 className="mb-20 text-xl md:text-center md:px-4 md:mb-12 md:text-base font-montserrat">
+        Thanks to our amazing sponsors for making AtomHacks possible!
+      </h1>
       <div className="flex flex-wrap items-center justify-center">
         {list.map((sponsor, i) => (
-          <div className="mx-12 my-8" key={i}>
-            <Image width={250} height={250} key={i} src={`/assets/sponsors/${sponsor.path}`} />
+          <div className="mx-24 my-8" key={i}>
+            <Link href={sponsor.link} passHref>
+              <Image width={250} height={250} key={i} src={`/assets/sponsors/${sponsor.path}`} />
+            </Link>
           </div>
         ))}
       </div>
