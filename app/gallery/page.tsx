706e8642dadcf729190cbbf0d13269fb34002608
Fahim <fahimhussain21@gmail.com>
2023-03-10 18:03:29 -0500

    'fix' gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,14 +1,13 @@
-"use client";
-
 import { ListObjectsV2Command } from "@aws-sdk/client-s3";
 import Image from "next/image";
-import { Transition, Dialog } from "@headlessui/react";
 import bucket from "../../lib/bucket";
-import { useState, Fragment, Key } from "react";
+import { Key } from "react";
 
 async function getPhotos() {
-  const items_2022 = await bucket.send(new ListObjectsV2Command({ Bucket: "atomhacks", Prefix: "Photos/2022/" }));
-  const items_2019 = await bucket.send(new ListObjectsV2Command({ Bucket: "atomhacks", Prefix: "Photos/2019/" }));
+  const [items_2022, items_2019] = await Promise.all([
+    bucket.send(new ListObjectsV2Command({ Bucket: "atomhacks", Prefix: "Photos/2022/" })),
+    bucket.send(new ListObjectsV2Command({ Bucket: "atomhacks", Prefix: "Photos/2022/" })),
+  ]);
   const photos = [
     items_2022!
       .Contents!.slice(1)
@@ -25,11 +24,11 @@ async function getPhotos() {
 
 export default async function Gallery() {
   const photos = await getPhotos();
-  const [selectedImage, setSelectedImage] = useState(null);
+  // const [selectedImage, setSelectedImage] = useState<null | string>(null);
 
   return (
     <div className="bg-zinc-900 p-8 font-montserrat text-white">
-      <Transition appear show={selectedImage != null} as={Fragment}>
+      {/* <Transition appear show={selectedImage != null} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={() => setSelectedImage(null)}>
           <Transition.Child
             as={Fragment}
@@ -63,21 +62,21 @@ export default async function Gallery() {
             </div>
           </div>
         </Dialog>
-      </Transition>
+      </Transition> */}
       <div className="mb-8 flex items-center justify-center">
         <span className="border-b-4 border-green-500 py-6 font-morro text-7xl md:text-5xl">GALLERY</span>
       </div>
       <h1 className="mb-3 inline-block border-b-4 border-yellow-500 text-4xl">2022</h1>
       <div className="flex justify-center">
         <div className="grid grid-cols-3 gap-8 sm:grid-cols-1 md:grid-cols-1">
-          {photos[0].map((photo: any, i: Key) => (
+          {photos[0].map((photo: string, i: Key) => (
             <Image
               className="hover:border-box hover:outline-3 outline-solid box-border cursor-pointer rounded-xl outline-green-500 transition duration-200 hover:outline"
               src={photo}
               width={620}
               height={200}
               key={i}
-              onClick={() => setSelectedImage(photo)}
+              // onClick={() => setSelectedImage(photo)}
               sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
               alt={""}
             />
@@ -87,14 +86,14 @@ export default async function Gallery() {
       <h1 className="mb-3 mt-4 inline-block border-b-4 border-yellow-500 text-4xl">2019</h1>
       <div className="flex justify-center">
         <div className="grid grid-cols-3 gap-8 sm:grid-cols-1 md:grid-cols-1">
-          {photos[1].map((photo: any, i: Key) => (
+          {photos[1].map((photo: string, i: Key) => (
             <Image
               className="hover:border-box hover:outline-3 outline-solid box-border cursor-pointer rounded-xl outline-green-500 transition duration-200 hover:outline"
               src={photo}
               width={620}
               height={200}
               key={i}
-              onClick={() => setSelectedImage(photo)}
+              // onClick={() => setSelectedImage(photo)}
               sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
               alt={""}
             />
