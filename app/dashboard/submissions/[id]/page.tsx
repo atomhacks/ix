4b52fed4cbf9713395d39a07d3d8cdc46b9c647d
Fahim Hussain <fahimhussain21@gmail.com>
2023-03-15 23:51:02 -0400

    kind of context menu

--- app/dashboard/submissions/[id]/page.tsx
+++ app/dashboard/submissions/[id]/page.tsx
@@ -1,7 +1,16 @@
-import { ArrowLongLeftIcon, ArrowLongRightIcon, PhotoIcon } from "@heroicons/react/24/outline";
+import { Popover, Transition } from "@headlessui/react";
+import {
+  ArrowLongLeftIcon,
+  ArrowLongRightIcon,
+  ChevronDownIcon,
+  EllipsisHorizontalIcon,
+  PhotoIcon,
+} from "@heroicons/react/24/outline";
 import { getServerSession } from "next-auth";
 import { notFound, redirect } from "next/navigation";
+import { Fragment } from "react";
 import { getSubmission } from "../../../../lib/server";
+import ActionsMenu from "./ActionsMenu";
 
 export default async function SubmissionPage({ params }: { params: { id: string } }) {
   const jwt = await getServerSession({
@@ -18,24 +27,27 @@ export default async function SubmissionPage({ params }: { params: { id: string 
     notFound();
   }
 
+  const isMine = submission.team.users.map((user) => user.id).some((id) => id == jwt.sub);
+
   return (
     <>
-      <div className="h-80 w-full py-8 bg-black flex justify-center items-center">
-          <button>
-            <ArrowLongLeftIcon className="w-8 h-8 text-white mr-8" />
-          </button>
-        <div className="bg-neutral-900 w-1/2 rounded-xl h-full flex justify-center items-center">
-<PhotoIcon className="w-8 h-8 text-neutral-400" />
+      <div className="flex h-96 w-full items-center justify-center bg-black py-8 sm:h-80">
+        <button>
+          <ArrowLongLeftIcon className="mr-8 h-8 w-8 text-white" />
+        </button>
+        <div className="flex h-full w-2/6 min-w-[600px] items-center justify-center rounded-xl bg-neutral-900">
+          <PhotoIcon className="h-8 w-8 text-neutral-400" />
         </div>
         <button>
-          <ArrowLongRightIcon className="w-8 h-8 text-white ml-8" />
+          <ArrowLongRightIcon className="ml-8 h-8 w-8 text-white" />
         </button>
       </div>
       <div className="flex justify-center">
-        <div className="max-w-screen-md mx-auto p-4">
+        <div className="max-w-screen-md ml-auto p-4">
           <h1 className="mb-4 text-6xl font-bold text-teal-300">{submission.name}</h1>
           <p className="text-xl">{submission.description}</p>
         </div>
+        <div className="m-4 ml-auto">{isMine && <ActionsMenu />}</div>
       </div>
     </>
   );
