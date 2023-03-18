ebeb25fb0bbad34f11385412789bbdc150be7f22
David Wang <wangd7@bxscience.edu>
2023-03-18 09:53:27 -0400

    Update with prizes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,14 +2,11 @@ import "server-only";
 
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import Link from "next/link";
 import { getUser } from "../../lib/server";
 import {
   CheckCircleIcon,
   ExclamationCircleIcon,
-  LinkIcon,
-  ListBulletIcon,
-  UsersIcon,
+  LinkIcon
 } from "@heroicons/react/24/outline";
 import SignIn from "../components/SignIn";
 
@@ -38,26 +35,9 @@ export default async function DashboardLanding() {
         <span className="py-6 border-b-4 border-green-500 font-morro text-7xl md:text-5xl">DASHBOARD</span>
       </div>
       <h1 className="p-4 text-xl text-center md:text-base">
-        Please make sure that you complete all the tasks before the event begins.
+        Please use the sidebar to create a team and submission.
       </h1>
       <div className="flex flex-col items-center justify-around gap-4">
-        <Link
-          className={`flex w-2/5 flex-row items-center rounded-lg border-2 bg-transparent p-4 md:w-4/5 ${
-            !user.formInfo ? "border-red-500" : "border-green-500"
-          }`}
-          href="/dashboard/form"
-        >
-          {" "}
-          <div className="object-contain w-10 h-10 md:h-5 md:w-5">
-            {" "}
-            <ListBulletIcon />
-          </div>
-          <h1 className="mx-4 text-2xl text-left grow md:text-sm">Complete the form</h1>
-          <div className="object-contain w-10 h-10 md:h-5 md:w-5">
-            {" "}
-            {!user.formInfo ? <ExclamationCircleIcon /> : <CheckCircleIcon />}
-          </div>
-        </Link>
         <SignIn
           provider="discord"
           callbackUrl="/dashboard"
@@ -80,23 +60,6 @@ export default async function DashboardLanding() {
             )}
           </div>
         </SignIn>
-        <Link
-          className={`flex w-2/5 flex-row items-center rounded-lg border-2 bg-transparent p-4 md:w-4/5 ${
-            !user.team ? "border-red-500" : "border-green-500"
-          }`}
-          href="/dashboard/team/create"
-        >
-          {" "}
-          <div className="object-contain w-10 h-10 md:h-5 md:w-5">
-            {" "}
-            <UsersIcon />
-          </div>
-          <h1 className="mx-4 text-2xl text-left grow md:text-sm">Create a team</h1>
-          <div className="object-contain w-10 h-10 md:h-5 md:w-5">
-            {" "}
-            {!user.team ? <ExclamationCircleIcon /> : <CheckCircleIcon />}
-          </div>
-        </Link>
       </div>
     </>
   );
--- app/dashboard/submissions/create/page.tsx
+++ app/dashboard/submissions/create/page.tsx
@@ -30,20 +30,20 @@ export default async function CreateSubmissionPage() {
       name: "GENERAL",
       description:
         "Everyone is automatically registered for the regular track. Do your best to stick to the theme of CENSORED.",
-      prizes: ["PENDING", "PENDING", "PENDING"],
+      prizes: ["Nintendo Switch Lite", "Drone", "$30 Amazon Gift Card"],
       value: "GENERAL",
     },
     {
       name: "Beginner",
       description: "Your project will be up against other beginners. All groupmates must be beginners.",
-      prizes: ["PENDING"],
+      prizes: ["Arduino Kit"],
       value: "BEGINNER",
     },
     {
       name: "Sponsorship Track",
-      description: "Courtesy of SPONSOR_PENDING! Your project must feature TECHNOLOGY_PENDING.",
-      prizes: ["SPONSOR_CREDITS"],
-      value: "SPONSOR_PENDING",
+      description: "Courtesy of Taskade! Your team must demonstrate your use of Taskade to be eligible.",
+      prizes: ["Taskade Lifetime Upgrades (Worth $99)"],
+      value: "Taskade",
     },
   ];
 
@@ -51,7 +51,7 @@ export default async function CreateSubmissionPage() {
     <div className="max-w-screen-xl mx-8 flex grow items-center justify-center rounded-lg bg-neutral-800 px-4 py-2">
       <div className="mr-12 flex basis-1/2 flex-col items-end justify-end">
         <h1 className="text-4xl font-bold">Create a submission</h1>
-        <h2 className="text-neutral-400">None of these options are permanant</h2>
+        <h2 className="text-neutral-400">You can edit these details at anytime before and after publishing your submission.</h2>
       </div>
       <div className="mt-2 ml-6 flex basis-1/2 items-start justify-start text-neutral-300">
         <CreateSubmissionForm tracks={tracks} />
