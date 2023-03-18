c91c0d3b5340577cca0b7f0ae84ebcfd14c19281
David Wang <wangd7@bxscience.edu>
2023-03-18 19:39:07 +0000

    taskade prisma migration

---
+++ prisma/migrations/20230318193848_add_taskade_track/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [SPONSOR_PENDING] on the enum `Track` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "Track_new" AS ENUM ('GENERAL', 'BEGINNER', 'TASKADE');
+ALTER TABLE "Submission" ALTER COLUMN "tracks" DROP DEFAULT;
+ALTER TABLE "Submission" ALTER COLUMN "tracks" TYPE "Track_new"[] USING ("tracks"::text::"Track_new"[]);
+ALTER TYPE "Track" RENAME TO "Track_old";
+ALTER TYPE "Track_new" RENAME TO "Track";
+DROP TYPE "Track_old";
+ALTER TABLE "Submission" ALTER COLUMN "tracks" SET DEFAULT ARRAY['GENERAL']::"Track"[];
+COMMIT;
